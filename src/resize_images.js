c212f0d3394b6a407053e504c174e3b0a090ccb1
sdennett55@gmail.com <sdennett55@gmail.com>
2019-07-31 00:09:30 -0400

    updated node, sharp, and all images to be resized

---
+++ src/resize_images.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var sharp = require('sharp');
+
+const files = fs.readdirSync(`${__dirname}/images`);
+const pathTo = `${__dirname}/images/`;
+const pathToOutput = `${__dirname}/images/output`;
+
+files.forEach(file => {
+  sharp(pathTo + file)
+    .toFile(pathToOutput + file).then(function (newFileInfo) {
+      console.log("Success");
+    })
+    .catch(function (err) {
+      console.log("Error occured", err);
+    });
+});
